b506fee1573846e6337d6078930daedd96b57e79
ankitkulhade0007 <ankitkulhade123@gmail.com>
2020-08-01 23:49:46 +0530

    my first commit

---
+++ src/test/java/org/EndToEndMaven/HelloSonar.java
@@ -0,0 +1,11 @@
+package org.EndToEndMaven;
+
+public class HelloSonar {
+
+	public static void main(String[] args) {
+		System.out.println("Hello Sonar");
+		int i = 500;
+		System.out.println("value fo a is "+i);
+	}
+
+}
